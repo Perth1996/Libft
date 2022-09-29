ba20ea7c678e222313858c7d62fbcb6a438ce7f4
Plawkiat <108067710+Perth1996@users.noreply.github.com>
2022-09-29 07:57:43 +0700

    Update ft_strncmp.c

--- str/ft_strncmp.c
+++ str/ft_strncmp.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+//comparing between 2 str only in n index
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
