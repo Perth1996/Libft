31ffb11e91c301932f405c5be28411f779273790
ODAIBA <108067710+Perth1996@users.noreply.github.com>
2022-10-01 13:16:45 +0700

    Update ft_putchar_fd.c

--- fd/ft_putchar_fd.c
+++ fd/ft_putchar_fd.c
@@ -10,7 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+//fd has 3 or more form to tell computer what are they have to do?
+//0 = input, 1 = output, 2 = error
 void	ft_putchar_fd(char c, int fd)
 {
 	write (fd, &c, 1);
