b11344cf69a86b1318b84aad8acb61cc594e44fb
ODAIBA <108067710+Perth1996@users.noreply.github.com>
2022-10-01 13:20:43 +0700

    Update ft_calloc.c

--- calloc/ft_calloc.c
+++ calloc/ft_calloc.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
+//use bzero and malloc
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
